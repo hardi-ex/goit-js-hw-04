23552ff33e1434072f633bf86c43c63e9f5a8aea
Yaroslav <160244855+hardi-ex@users.noreply.github.com>
2024-04-28 22:03:37 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+  let totalNumbers = [];
+
+  for (const item of numbers) {
+    if (value < item) {
+      totalNumbers.push(item);
+    }
+  }
+
+  return totalNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
